209fffeb9819d5152f7623e93f49f399676a88c3
Fernando Rocha <frochaolivera18@hotmail.com>
2023-11-12 21:27:15 -0500

    first commit

---
+++ orders-service/src/test/java/com/javamicro/orders_service/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamicro.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
